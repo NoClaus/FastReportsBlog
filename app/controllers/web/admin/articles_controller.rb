98f44d4865cb1dcd3f31875a5a8385b8a191ab0a
NoClaus <ralko.stanislav@gmail.com>
2023-01-22 08:05:02 -0500

    Rubocop policy

--- app/controllers/web/admin/articles_controller.rb
+++ app/controllers/web/admin/articles_controller.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class Web::Admin::ArticlesController < ApplicationController
-  def index; 
+  def index
     @articles = Article.all
-                       .order(created_at: :desc)  
+                       .order(created_at: :desc)
                        .page(page)
                        .per(per_page)
   end
@@ -28,16 +28,15 @@ class Web::Admin::ArticlesController < ApplicationController
   end
 
   def update
-
     change_state(@article)
-      if @article.update article_params
-        redirect_to @article, notice: t('.success')
-      else
-        render :edit, status: :unprocessable_entity
+    if @article.update article_params
+      redirect_to @article, notice: t('.success')
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
-  def destroy; 
+  def destroy
     @article = Article.find(params[:id])
     @article.destroy
     redirect_to admin_root_path

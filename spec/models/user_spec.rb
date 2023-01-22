4c6276fb5c11ad090a8f0ae5d59cd23337d9cd1c
NoClaus <ralko.stanislav@gmail.com>
2023-01-22 06:00:51 -0500

    Installed Shoulda Matchers

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'associations' do
-    it { is_expected.to have_many(:links).dependent(:destroy) }
+    it { is_expected.to have_many(:articles).dependent(:destroy) }
   end
 end

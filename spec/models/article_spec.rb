f2d7434690ccca7ea1965237e9cc8d83eb522ee9
NoClaus <ralko.stanislav@gmail.com>
2023-01-23 05:14:06 -0500

    Fixed Article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -4,15 +4,15 @@ require 'rails_helper'
 
 RSpec.describe Article do
   describe 'associations' do
-    it { is_expected.to belong_to(:user) }
+    it { should belong_to(:user) }
   end
 
   describe 'validations' do
-    it { is_expected.to validate_attached_of(:photo) }
-    it { is_expected.to validate_content_type_of(:photo).allowing('image/png') }
-    it { is_expected.to validate_size_of(:photo).less_than_or_equal_to(5.megabyte) }
-    it { is_expected.to validate_presence_of(:title) }
-    it { is_expected.to validate_presence_of(:body) }
-    it { is_expected.to validate_length_of(:body).is_at_least(50) }
+    it { should have_one_attached(:photo) }
+    it { should validate_content_type_of(:photo).allowing('image/png') }
+    it { should validate_size_of(:photo).less_than_or_equal_to(5.megabyte) }
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:body) }
+    it { should validate_length_of(:body).is_at_least(50) }
   end
 end
